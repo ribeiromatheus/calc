d6eb0d5196e4193198a1e7dd2608da0b8602af45
Matheus Ribeiro <ribeiro.matheuspereiraribeiro@gmail.com>
2024-07-03 17:51:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,9 @@ function calcularPesoLiquido() {
   let pesoBruto = parseFloat(document.getElementById('pesoBruto').value);
   let quantidadeCaixas = parseInt(document.getElementById('quantidadeCaixas').value);
   let taraTotal = parseFloat(document.getElementById('taraTotal').value);
-  let pesoPallet = parseFloat(document.getElementById('pesoPallet').value);
+  let pesoPalletInput = document.getElementById('pesoPallet').value.trim();
+  let pesoPallet = pesoPalletInput === '' ? 0 : parseFloat(pesoPalletInput);
 
   // Calculando o peso líquido
   let pesoLiquido = pesoBruto - (quantidadeCaixas * taraTotal) - pesoPallet;
-
-  // Verificando se o resultado é um número válido
-  if (!isNaN(pesoLiquido)) {
-    // Exibindo o resultado
-    document.getElementById('resultado').innerHTML = `Peso Líquido: ${pesoLiquido.toFixed(2)} kg`;
-  } else {
-    document.getElementById('resultado').innerHTML = 'Preencha todos os campos corretamente.';
-  }
 }
